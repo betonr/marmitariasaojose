ca55d12cd2ac4c04bdf8ef11b5d15bba3b8337b6
betonr <carlos.noronha@fatec.sp.gov.br>
2018-05-30 10:35:13 -0300

    update getCPF closures

--- WebContent/static/js/getCEP.js
+++ WebContent/static/js/getCEP.js
@@ -1,26 +1,29 @@
 var inputCep = document.getElementById("cep");
 
+function getInfos(cep){
+	
+	var url = 'https://viacep.com.br/ws/' + cep + '/json/';
+	
+	return $.get(url, function(data) {
+
+        if (!("erro" in data)) {
+        
+        	$('input[name="rua"]').val(data.logradouro);
+        	$('input[name="bairro"]').val(data.bairro);
+        	$('input[name="cidade"]').val(data.localidade);
+        	
+        } else {
+          alert("CEP não encontrado.");
+        }
+    })
+}
+
 inputCep.addEventListener('blur', function(e) {
 
-	  var cep = $('#cep').val().replace(/\D/g, '');
+	  var cep = e.target.value.replace(/\D/g, '');
 	
 	  if (cep !== "") {
-	
-	    var url = 'https://viacep.com.br/ws/' + cep + '/json/';
-	    
-	    $.get(url, function(data) {
-
-	        if (!("erro" in data)) {
-	        
-	        	$('input[name="rua"]').val(data.logradouro);
-	        	$('input[name="bairro"]').val(data.bairro);
-	        	$('input[name="cidade"]').val(data.localidade);
-	        	
-	        } else {
-	          alert("CEP não encontrado.");
-	        }
-	    });
-	    
+		  getInfos(cep)    	    
 	  }
   
 });
